a1ea98a6930ddd7a3d3107e069d05a4f8ee662d7
Aaj <aaj1011@qq.com>
2024-04-13 16:22:52 +0800

    [ADD] 增加自动化编译和执行程序脚本
    1.自测通过，可以正常编译和执行，执行前会杀死旧程序，程序会在后台执行

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+app_path="/home/workspace/myBlog/myBlogWeb/server"
+
+# 获取pid，用于停止程序
+pid=$(pgrep myBlog)
+
+#if [ $pid -eq 0 ]; then
+#    # 程序在运行
+#    kill $pid
+#
+#fi
+
+while [ $pid > /dev/null ]
+do
+    echo "myBlog pid is :$pid, it will be kill and restart soon"
+    kill $pid
+    pid=$(pgrep myBlog)
+done
+
+cd "$app_path"; rm -rf myBlog; go build -o myBlog;  ./myBlog &
+echo "myBlog is running!"
+
+
